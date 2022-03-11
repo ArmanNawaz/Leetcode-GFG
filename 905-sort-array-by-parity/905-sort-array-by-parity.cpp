0bf5b992e00f940b2f4cfd62cb854173177ac18d
ArmanNawaz <77883235+ArmanNawaz@users.noreply.github.com>
2022-03-11 11:13:28 +0530

    Time: 25 ms (15.04%), Space: 16.3 MB (55.25%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.cpp
+++ 905-sort-array-by-parity/905-sort-array-by-parity.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
+        return solve(nums);
         return bruteforce(nums);
     }
 private:
@@ -18,6 +19,18 @@ private:
         
         for(int n: odds)
             nums[i++] = n;
+        return nums;
+    }
+    
+    vector<int> solve(vector<int>& nums) {
+        int i = 0, j = nums.size()-1;
+        
+        while(i < j) {
+            while(i < nums.size() && nums[i] % 2 == 0) ++i;
+            while(j > 0 && nums[j] % 2 != 0) --j;
+            
+            if(i < j) swap(nums[i], nums[j]);
+        }
         return nums;
     }
 };
