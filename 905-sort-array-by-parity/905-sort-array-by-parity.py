30b5e8b2f58e07673695300f8afa3a82754e1b50
ArmanNawaz <77883235+ArmanNawaz@users.noreply.github.com>
2022-03-11 11:21:24 +0530

    Time: 104 ms (61.66%), Space: 14.6 MB (75.74%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.py
+++ 905-sort-array-by-parity/905-sort-array-by-parity.py
@@ -2,19 +2,32 @@ class Solution:
     def sortArrayByParity(self, nums: List[int]) -> List[int]:
         
         # O(n) Time and O(1) extra space
-        odds = []
-        evens = []
+#         odds = []
+#         evens = []
         
-        for n in nums:
-            if n % 2 == 0:
-                evens.append(n)
-            else:
-                odds.append(n)
-        i = 0
-        for n in evens:
-            nums[i] = n
-            i += 1
-        for n in odds:
-            nums[i] = n
-            i += 1
+#         for n in nums:
+#             if n % 2 == 0:
+#                 evens.append(n)
+#             else:
+#                 odds.append(n)
+#         i = 0
+#         for n in evens:
+#             nums[i] = n
+#             i += 1
+#         for n in odds:
+#             nums[i] = n
+#             i += 1
+#         return nums
+
+        
+        # O(n) Time and O(1) extra space
+        
+        i, j = 0, len(nums) - 1
+        
+        while i < j:
+            while i < len(nums) and nums[i] % 2 == 0: i += 1
+            while j >= 0 and nums[j] % 2 != 0: j -= 1
+            
+            if i < j:
+                nums[i], nums[j] = nums[j], nums[i]
         return nums
