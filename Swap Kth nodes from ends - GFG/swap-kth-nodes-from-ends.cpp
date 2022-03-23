f1f1e6fa88993a9cac8bacaec2701d2902e9f6e4
ArmanNawaz <77883235+ArmanNawaz@users.noreply.github.com>
2022-03-23 23:14:52 +0530

    Added solution - LeetHub

---
+++ Swap Kth nodes from ends - GFG/swap-kth-nodes-from-ends.cpp
@@ -0,0 +1,107 @@
+// { Driver Code Starts
+#include <iostream>
+
+using namespace std;
+
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+
+
+Node *swapkthnode(Node* head, int num, int K);
+
+void addressstore(Node **arr, Node* head)
+{
+    Node* temp = head;
+    int i = 0;
+    while(temp){
+        arr[i] = temp;
+        i++;
+        temp = temp->next;
+    }
+}
+
+bool check(Node ** before, Node **after, int num, int K)
+{
+    if(K > num)
+        return 1;
+    return (before[K-1] == after[num - K]) && (before[num-K] == after[K-1]);
+}
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--){
+        int num, K , firstdata;
+        cin>>num>>K;
+        int temp;
+        cin>>firstdata;
+        Node* head = new Node(firstdata);
+        Node* tail = head;
+        for(int i = 0; i<num - 1; i++){
+            cin>>temp;
+            tail->next = new Node(temp);
+            tail = tail->next;
+        }
+        
+        Node *before[num];
+        addressstore(before, head);
+        
+        head = swapkthnode(head, num, K);
+        
+        Node *after[num];
+        addressstore(after, head);
+        
+        if(check(before, after, num, K))
+            cout<<1<<endl;
+        else
+            cout<<0<<endl;
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for C++
+
+/* Linked List Node structure
+   struct Node  {
+     int data;
+     Node *next;
+     Node(int x) {
+        data = x;
+        next = NULL;
+  }
+  }
+*/
+
+//Function to swap Kth node from beginning and end in a linked list.
+Node *swapkthnode(Node* head, int num, int K) {
+    Node* temp = new Node(-1);
+    Node* t1 = temp;
+    Node* t2 = temp;
+    
+    temp -> next = head;
+    
+    while(--K) {
+        t1 = t1 -> next;
+        if(t1 -> next == NULL)
+            return head;
+    }
+    Node * tmp = t1 -> next;
+    
+    while(tmp -> next != NULL) {
+        tmp = tmp -> next;
+        t2 = t2 -> next;
+    }
+    swap(t1 -> next, t2 -> next);
+    swap(t1 -> next -> next, t2 -> next -> next);
+    
+    return temp -> next;
+}
