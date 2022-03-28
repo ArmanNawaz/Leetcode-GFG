56963142abb17b652cd1e74a14c7512ece900c5f
ArmanNawaz <77883235+ArmanNawaz@users.noreply.github.com>
2022-03-28 17:44:59 +0530

    Time: 8 ms (38.76%), Space: 12.2 MB (37.08%) - LeetHub

---
+++ 31-next-permutation/31-next-permutation.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    
+public:
+    void nextPermutation(vector<int>& nums) {
+        int threshold = -1, localMaxima;
+        for(int i = nums.size()-1; i > 0 ; i--){
+            if(nums[i] > nums[i-1]){
+                threshold = i-1;
+                break;
+            } 
+        }
+        if(threshold == -1) 
+            reverse(nums.begin(), nums.end());
+        else{
+            for(int i = nums.size() - 1; i >= 0; i--){
+                if(nums[i] > nums[threshold]){
+                    swap(nums[i], nums[threshold]);
+                    break;
+                }
+            }
+            if(threshold < nums.size()-1)
+            reverse(nums.begin() + threshold + 1, nums.end());
+        }
+    }
+};
