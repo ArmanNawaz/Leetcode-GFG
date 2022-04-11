97976ef56120c5496d9e8bb5f2d406723f9eccd8
ArmanNawaz <77883235+ArmanNawaz@users.noreply.github.com>
2022-04-11 18:52:59 +0530

    Time: 464 ms (37.04%), Space: 16.4 MB (62.07%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,29 +1,28 @@
 class Solution:
-    def dfs(self, grid, x, y, visited):
+    def dfs(self, grid, x, y):
+        grid[x][y] = '0'
         
-        visited[x][y] = True
-        
-        if x >= 1 and grid[x - 1][y] == '1' and visited[x - 1][y] == False:
-            self.dfs(grid, x - 1, y, visited)
+        if x >= 1 and grid[x - 1][y] == '1':
+            self.dfs(grid, x - 1, y)
             
-        if x < len(grid) - 1 and grid[x + 1][y] == '1' and visited[x + 1][y] == False:
-            self.dfs(grid, x + 1, y, visited)
+        if x < len(grid) - 1 and grid[x + 1][y] == '1':
+            self.dfs(grid, x + 1, y)
             
-        if y >= 1 and grid[x][y - 1] == '1' and visited[x][y - 1] == False:
-            self.dfs(grid, x, y - 1, visited)
+        if y >= 1 and grid[x][y - 1] == '1':
+            self.dfs(grid, x, y - 1)
             
-        if y < len(grid[0]) - 1 and grid[x][y + 1] == '1' and visited[x][y + 1] == False:
-            self.dfs(grid, x, y + 1, visited)
+        if y < len(grid[0]) - 1 and grid[x][y + 1] == '1':
+            self.dfs(grid, x, y + 1)
         
     
     def numIslands(self, grid: List[List[str]]) -> int:
         ans = 0
         
-        visited = [[False] * len(grid[0]) for _ in range(len(grid))]
+        # visited = [[False] * len(grid[0]) for _ in range(len(grid))]
         
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if (visited[i][j] == False) and (grid[i][j] == '1'):
-                    self.dfs(grid, i, j, visited)
+                if grid[i][j] == '1':
+                    self.dfs(grid, i, j)
                     ans += 1
         return ans
