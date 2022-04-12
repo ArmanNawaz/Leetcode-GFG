5353682d3ff1f0a8c065e73b72e5b5fc6bdc4fef
ArmanNawaz <77883235+ArmanNawaz@users.noreply.github.com>
2022-04-12 23:25:29 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (96.30%) - LeetHub

--- 289-game-of-life/289-game-of-life.cpp
+++ 289-game-of-life/289-game-of-life.cpp
@@ -23,16 +23,13 @@ private:
                 if(i < n - 1 && j > 0 && board[i + 1][j - 1] >= 1) cnt++; 
                 if(i < n - 1 && j < m - 1 && board[i + 1][j + 1] >= 1) cnt++; 
                 
-                // Replacing live state (1) as dead state (0) as 2 if cnt<2 OR cnt>3
-                // Replacing dead state (0) as alive state (0) as -1 if cnt==3
-				
+                
                 if(board[i][j] == 1 && (cnt < 2 || cnt > 3)) board[i][j] = 2;
                 
                 if(board[i][j] == 0 && cnt == 3) board[i][j] = -1;
             }
         }
         
-		// Replacing all the 2s to 0 and -1s to 1
         for(int i = 0; i < board.size(); i++){
             for(int j = 0; j < board[0].size(); j++){
                 if(board[i][j] == 2) board[i][j] = 0;
