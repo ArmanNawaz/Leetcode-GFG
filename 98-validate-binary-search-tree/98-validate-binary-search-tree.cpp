21968525857a3f01fafb53bb833e4bc4b6023f7a
ArmanNawaz <77883235+ArmanNawaz@users.noreply.github.com>
2022-04-14 14:14:01 +0530

    Time: 19 ms (42.54%), Space: 21.9 MB (15.30%) - LeetHub

---
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        return bruteForce(root);
+        
+        // return solve(root);
+    }
+private:   
+    bool bruteForce(TreeNode* root) {
+        vector<int> inorder;
+        getInorder(root, inorder);
+        
+        for(int i = 0; i < inorder.size() - 1; ++i) {
+            if(inorder[i] >= inorder[i + 1])
+                return false;
+        }
+        return true;
+    }
+    
+    void getInorder(TreeNode* root, vector<int>& inorder) {
+        if(root) {
+            getInorder(root -> left, inorder);
+            inorder.push_back(root -> val);
+            getInorder(root -> right, inorder);
+        }
+    }
+    
+    
+    
+    
+    
+    bool solve(TreeNode* root) {
+        if(root == nullptr)
+            return true;
+         
+        if((root -> left != nullptr && root -> val <= root -> left -> val) || (root -> right != nullptr && root -> val >= root -> right -> val))
+            return false;
+        
+        return solve(root -> left) && solve(root -> right);
+    }
+};
