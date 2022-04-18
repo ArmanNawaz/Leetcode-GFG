7c6a643ac93a870f85fbc361356e5ffc7ac388f9
ArmanNawaz <77883235+ArmanNawaz@users.noreply.github.com>
2022-04-18 12:05:38 +0530

    Time: 12 ms (74.80%), Space: 9.1 MB (38.09%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,12 +1,36 @@
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-        int m = word1.length(), n = word2.length();
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
+//         int m = word1.length(), n = word2.length();
+//         vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
         
-        return solve(word1, word2, m, n, dp);
+//         return solve(word1, word2, m, n, dp);
+        
+        return tabulation(word1, word2);
     }
 private:
+    // Rahul Sir
+    int tabulation(string& s1, string& s2) {
+        int m = s1.length(), n = s2.length();
+        
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
+        
+        for(int i = 0; i <= n; ++i)
+            dp[0][i] = i;
+        for(int i = 0; i <= m; ++i)
+            dp[i][0] = i;
+        
+        for(int i = 1; i <= m; ++i){
+            for(int j = 1; j <= n; ++j) {
+                if(s1[i - 1] == s2[j - 1])
+                    dp[i][j] = dp[i - 1][j - 1];
+                else
+                    dp[i][j] = min(dp[i - 1][j - 1], min(dp[i - 1][j], dp[i][j - 1])) + 1;
+            }
+        }
+        return dp[m][n];
+    }
+    
     int solve(string& s1, string& s2, int m, int n, vector<vector<int>>& dp) {
         if(s1.length() == 0)
             return dp[m][n] = n;
