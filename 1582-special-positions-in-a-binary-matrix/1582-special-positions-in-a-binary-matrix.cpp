581f64d101f814c52f4dd616723c78864e3416bd
ArmanNawaz <77883235+ArmanNawaz@users.noreply.github.com>
2022-04-19 19:08:44 +0530

    Time: 26 ms (65.44%), Space: 13 MB (31.10%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int numSpecial(vector<vector<int>>& mat) {
-        return bruteForce(mat);
+        return optimised(mat);
+        // return bruteForce(mat);
     }
 private:
     // O(mn * (m + n)) Time and O(1) extra space
@@ -25,6 +26,33 @@ private:
                         ++col;
                     }
                     if(row == m && col == n) ++result;
+                }
+            }
+        }
+        return result;
+    }
+    
+    // O(mn) Time and O(m + n) extra space
+    int optimised(vector<vector<int>>& mat) {
+        int m = mat.size(), n = mat[0].size();
+        int result = 0;
+        vector<int> row(m, 0), col(n, 0);
+        
+        for(int i = 0; i < m; ++i) {
+            for(int j = 0; j < n; ++j) {
+                if(mat[i][j] == 1){
+                    ++row[i];
+                    ++col[j];
+                }
+            }
+        }
+        
+        for(int i = 0; i < m; ++i) {
+            if(row[i] > 1) continue;
+            for(int j = 0; j < n; ++j) {
+                if(mat[i][j] == 1) {
+                    if(row[i] == 1 && col[j] == 1) ++result;
+                    break;
                 }
             }
         }
