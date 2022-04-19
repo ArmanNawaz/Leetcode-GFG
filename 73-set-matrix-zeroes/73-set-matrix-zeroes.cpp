a7df1b3ea809fee94ffc04a12f54993aef755653
ArmanNawaz <77883235+ArmanNawaz@users.noreply.github.com>
2022-04-19 19:27:32 +0530

    Time: 20 ms (48.00%), Space: 13.2 MB (54.63%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        return bruteForce(matrix);
+        return optimised(matrix);
+        // return bruteForce(matrix);
     }
 private:
     // O(mn * (m + n)) Time and O(mn) extra space
@@ -18,6 +19,36 @@ private:
                 }
             }
         }
+        return;
+    }
+    
+    void optimised(vector<vector<int>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size();
+        
+        vector<bool> rows(m, false), cols(n, false);
+        
+        for(int i = 0; i < m; ++i) {
+            for(int j = 0; j < n; ++j) {
+                if(matrix[i][j] == 0) {
+                    rows[i] = true;
+                    cols[j] = true;
+                }
+            }
+        }
+        
+        for(int i = 0; i < m; ++i) {
+            if(rows[i]) {
+                for(int j = 0; j < n; ++j) 
+                    matrix[i][j] = 0;
+            }
+        }
+        for(int j = 0; j < n; ++j) {
+            if(cols[j]) {
+                for(int i = 0; i < m; ++i) 
+                    matrix[i][j] = 0;
+            }
+        }
+        
         return;
     }
 };
