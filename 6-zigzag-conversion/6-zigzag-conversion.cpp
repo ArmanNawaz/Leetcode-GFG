baa637a73655f5ccf74b5a032b6dbdba406d2775
ArmanNawaz <77883235+ArmanNawaz@users.noreply.github.com>
2022-04-24 00:03:02 +0530

    Time: 261 ms (5.01%), Space: 162.8 MB (5.02%) - LeetHub

--- 6-zigzag-conversion/6-zigzag-conversion.cpp
+++ 6-zigzag-conversion/6-zigzag-conversion.cpp
@@ -26,16 +26,10 @@ private:
                 i += 2;
                 dir *= -1;
             }
-            // if(i < 0) i = 0;
             // If direction is upward then change column
             if(dir == -1)
                 ++j;
         }
-        // for(auto x: res) {
-        //     for(auto y: x)
-        //         cout<<y<<" ";
-        //     cout<<endl;
-        // } 
         string ans = "";
         for(auto row: res) {
             for(auto ch: row) {
