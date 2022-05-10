f3f19704ef4a70680a6b91e72c05468595f36ef1
ArmanNawaz <77883235+ArmanNawaz@users.noreply.github.com>
2022-05-10 23:55:43 +0530

    Time: 11 ms (78.31%), Space: 19.8 MB (82.60%) - LeetHub

---
+++ 113-path-sum-ii/113-path-sum-ii.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        return solve(root, targetSum);
+    }
+private:
+    void backtrack(TreeNode* root, int targetSum, vector<int>& partialAns, vector<vector<int>>& ans) {
+        if(root == nullptr)
+            return;
+        
+        targetSum -= root -> val;
+        partialAns.push_back(root -> val);
+        
+        if(root -> left == nullptr && root -> right == nullptr) {
+            if(targetSum == 0)
+                ans.push_back(partialAns);
+            partialAns.pop_back();
+            return;
+        }
+        
+        
+        if(root -> left) backtrack(root -> left, targetSum, partialAns, ans);
+        if(root -> right) backtrack(root -> right, targetSum, partialAns, ans);
+        
+        partialAns.pop_back();
+        targetSum += root -> val;
+        return;
+    }
+    
+    
+    vector<vector<int>> solve(TreeNode* root, int targetSum) {
+        vector<int> partialAns;
+        vector<vector<int>> ans;
+        
+        backtrack(root, targetSum, partialAns, ans);
+        
+        return ans;
+    }
+}; 
