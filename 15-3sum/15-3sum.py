df3531d8f714b466b7d5d0f8ef76948ddb3eab40
ArmanNawaz <77883235+ArmanNawaz@users.noreply.github.com>
2022-05-10 18:21:38 +0530

    Time: 740 ms (88.61%), Space: 18.1 MB (42.35%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,27 +1,36 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        n = len(nums)
         ans = []
-        for i in range(n):
-            j = i + 1
-            k = n - 1
+        
+        i, n = 0, len(nums)
+        
+        # iterate all the elements
+        while i < n:
+            j, k = i + 1, n - 1
             
+            # find the required sum in nums[i + 1: ]
             while j < k:
-                res = []
-                s = nums[i] + nums[j] + nums[k]
-                if s == 0:
-                    res.append(nums[i])
-                    res.append(nums[j])
-                    res.append(nums[k])
+                _sum = nums[i] + nums[j] + nums[k]
+                
+                if _sum < 0:
                     j += 1
+                    
+                elif _sum > 0:
                     k -= 1
-                elif s > 0:
-                    k -= 1
+                    
                 else:
-                    j += 1
-                if len(res) != 0 and res not in ans:
-                    ans.append(res)
+                    ans.append([nums[i], nums[j], nums[k]])
+                    
+                    # if the array contains duplicates
+                    # move the next different element
+                    pre, nxt = nums[j], nums[k]
+                    while j < n and k > i and pre == nums[j] and nxt == nums[k]:
+                        j += 1
+                        k -= 1
+            # move to the next different element
+            tmp = nums[i]
+            while i < n and tmp == nums[i]:
+                i += 1
                 
         return ans
-            
