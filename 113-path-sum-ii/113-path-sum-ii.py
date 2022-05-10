a6a1083a5bd70f842eac8606610ab9e148300195
ArmanNawaz <77883235+ArmanNawaz@users.noreply.github.com>
2022-05-11 00:08:07 +0530

    Time: 46 ms (86.54%), Space: 15.6 MB (74.00%) - LeetHub

---
+++ 113-path-sum-ii/113-path-sum-ii.py
@@ -0,0 +1,50 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def backtrack(self, root, targetSum):
+        
+        # include the current root value
+        targetSum -= root.val
+        self.partialAns.append(root.val)
+        
+        # if we are at leaf node or the path sum has exceeded the target
+        if root.left == None and root.right == None:
+            
+            # if path sum is equal to targetSum
+            if targetSum == 0:
+                self.ans.append(self.partialAns[:])
+            
+            # now pop the leaf element
+            # this operation is done here because we are returning from here
+            # and control is not reaching line - 37, 38 for the same
+            targetSum += root.val
+            self.partialAns.pop()
+            return
+        
+        # if left subtree is not none
+        if root.left != None:
+            self.backtrack(root.left, targetSum)
+        
+        # if right subtree is not none
+        if root.right != None:
+            self.backtrack(root.right, targetSum)
+        
+        # backtracking step
+        targetSum += root.val
+        self.partialAns.pop()
+        
+        
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+        if root == None:
+            return []
+        
+        self.partialAns = []
+        self.ans = []
+        
+        self.backtrack(root, targetSum)
+        
+        return self.ans
