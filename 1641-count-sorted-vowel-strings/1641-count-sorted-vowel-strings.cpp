b64d54abbd95b47ed78a0ccc2a126e024f170383
ArmanNawaz <77883235+ArmanNawaz@users.noreply.github.com>
2022-05-12 18:00:50 +0530

    Time: 112 ms (7.59%), Space: 6 MB (58.59%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -23,4 +23,32 @@ private:
         
         return inc + exc;
     }
+    
+    int recurse2(int n, int idx, vector<vector<int>>& dp) {
+        
+        if(dp[n][idx] != INT_MIN) {
+            return dp[n][idx];
+        }
+        
+        // if n size string is formed or no more vowel left to include
+        if(idx >= 5 || n == 0) {
+            if(n == 0)
+                return 1;
+            return 0;
+        }
+        
+        
+        // include current character and move for next set of vowels i.e, "aeiou"
+        int inc = recurse(n - 1, idx);
+        
+        // exclude current character and recurse for next character
+        int exc = recurse(n, idx + 1);
+        
+        return inc + exc;
+    }
+    int solve(int n) {
+        vector<vector<int>> dp(n + 1, vector<int>(5, INT_MIN));
+        
+        return recurse2(n, 0, dp);
+    }
 };
