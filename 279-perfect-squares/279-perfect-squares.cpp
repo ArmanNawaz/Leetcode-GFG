8764ab1a729d8890466631b07006bdee667bcde8
ArmanNawaz <77883235+ArmanNawaz@users.noreply.github.com>
2022-05-16 12:24:10 +0530

    Time: 445 ms (17.08%), Space: 8.9 MB (72.75%) - LeetHub

---
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        return solve(n);
+    }
+private:
+    int solve(int n) {
+        vector<int> dp(n + 1, INT_MAX);
+        dp[0] = 0;
+        dp[1] = 1;
+        
+        for(int i = 2; i <= n; ++i) {
+            for(int j = 1; j * j <= i; ++j) {
+                int rem = i - j * j;
+                
+                dp[i] = min(dp[i], dp[rem] + 1);
+            }
+        }
+        return dp[n];
+    }
+};
