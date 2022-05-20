e7faac76ea5205b6739e0da94f8259da89207216
ArmanNawaz <77883235+ArmanNawaz@users.noreply.github.com>
2022-05-20 23:00:33 +0530

    Added solution - LeetHub

--- A Special Keyboard - GFG/a-special-keyboard.cpp
+++ A Special Keyboard - GFG/a-special-keyboard.cpp
@@ -12,9 +12,11 @@ class Solution {
   public:
     int findTime(string S1, string S2) {
         // code here
-        return solve(S1, S2);
+        return solve1(S1, S2);
+        // return solve(S1, S2);
     }
 private:
+    // O(n) Time and O(n) space 
     int solve(string s1, string s2) {
         unordered_map<char, int> hashMap;
         
@@ -28,6 +30,22 @@ private:
         for(char ch: s2) {
             ans += abs(currIdx - hashMap[ch]);
             currIdx = hashMap[ch];
+        }
+        return ans;
+    }
+    
+    int solve1(string s1, string s2) {
+        int ans = 0;
+        int currIdx = 0;
+        
+        for(int i = 0; i < s2.length(); ++i) {
+            for(int j = 0; j < 26; ++j) {
+                if(s2[i] == s1[j]) {
+                    ans += abs(currIdx - j);
+                    currIdx = j;
+                    break;
+                }
+            }
         }
         return ans;
     }
