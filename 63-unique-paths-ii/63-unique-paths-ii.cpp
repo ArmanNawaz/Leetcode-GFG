b386559bea6ac87e41cf1d3735a2ef9f185b05ff
ArmanNawaz <77883235+ArmanNawaz@users.noreply.github.com>
2022-05-20 19:10:16 +0530

    Time: 9 ms (16.96%), Space: 7.9 MB (41.40%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -5,6 +5,17 @@ public:
         // return channaSir(obstacleGrid);
     }
 private:
+    int solve(int i, int j, vector<vector<int>>& dp) {
+        if(i == 0 && j == 0) return 1;
+        
+        
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        if(i == 0) return dp[i][j] = solve(i, j - 1, dp);
+        else if(j == 0) return dp[i][j] = solve(i - 1, j, dp);
+        
+        else return dp[i][j] = solve(i - 1, j, dp) + solve(i, j - 1, dp);
+    }
     int channaSir(vector<vector<int>>& obstacleGrid) {
         int m = obstacleGrid.size(), n = obstacleGrid[0].size();
         vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
@@ -18,19 +29,9 @@ private:
         }        
         return solve(m - 1, n - 1, dp);   
     }
-    int solve(int i, int j, vector<vector<int>>& dp) {
-        if(i == 0 && j == 0) return 1;
-        
-        
-        if(dp[i][j] != -1) return dp[i][j];
-        
-        if(i == 0) return dp[i][j] = solve(i, j - 1, dp);
-        else if(j == 0) return dp[i][j] = solve(i - 1, j, dp);
-        
-        else return dp[i][j] = solve(i - 1, j, dp) + solve(i, j - 1, dp);
-    }
     
     
+    // Daily-me
     int recurse(vector<vector<int>>& obstacleGrid, int i, int j, vector<vector<int>>& dp) {
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
