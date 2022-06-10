e1a722c941ee184b9da42212787d7c41ea91c9ff
ArmanNawaz <77883235+ArmanNawaz@users.noreply.github.com>
2022-06-10 23:41:42 +0530

    Time: 22 ms (59.36%), Space: 8.5 MB (44.95%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,35 +1,21 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        return solve1(s);
+        return daily(s);
     }
 private:
-    int solve1(string s) {
+    int daily(string s) {
         unordered_map<char, int> hashMap;
-        int wStart = 0, wEnd;
+        
+        int wStart = 0;
         int maxLen = 0;
         
-        for(wEnd = 0; wEnd < s.size(); ++wEnd) {
-            if(hashMap.find(s[wEnd]) != hashMap.end()) {
+        for(int wEnd = 0; wEnd < s.length(); ++wEnd) {
+            if(hashMap.find(s[wEnd]) != hashMap.end())
                 wStart = max(wStart, hashMap[s[wEnd]] + 1);
-            }
             hashMap[s[wEnd]] = wEnd;
             maxLen = max(maxLen, wEnd - wStart + 1);
         }
         return maxLen;
-    }
-    
-    int solve(string s){
-        unordered_map<char, int> hashMap;
-        int wStart = 0, wEnd, n = s.size();
-        int maxLength = 0;
-        for(wEnd = 0; wEnd < n; wEnd++){
-            if(hashMap.find(s[wEnd]) != hashMap.end()){
-                wStart = max(wStart, hashMap[s[wEnd]] + 1);
-            }
-            maxLength = max(maxLength, wEnd - wStart + 1);
-            hashMap[s[wEnd]] = wEnd;
-        }
-        return maxLength;
     }
 };
