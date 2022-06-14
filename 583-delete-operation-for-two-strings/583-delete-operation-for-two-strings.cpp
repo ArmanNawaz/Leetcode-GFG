ebcf3a74da81c460c4d4f14c07ed386d3bc31ed6
ArmanNawaz <77883235+ArmanNawaz@users.noreply.github.com>
2022-06-14 22:17:32 +0530

    Time: 46 ms (25.24%), Space: 12.3 MB (17.03%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -1,22 +1,31 @@
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-        int m = word1.length(), n = word2.length();
-        
-        return m + n - 2 * findLCS(word1, word2, m, n);
+        return daily(word1, word2);
     }
 private:
-    int findLCS(string word1, string word2, int m, int n) {
-        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+    int lcs(string& word1, string& word2) {
+        int m = word1.length();
+        int n = word2.length();
+        
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
         
         for(int i = 1; i <= m; ++i) {
             for(int j = 1; j <= n; ++j) {
                 if(word1[i - 1] == word2[j - 1])
                     dp[i][j] = 1 + dp[i - 1][j - 1];
                 else
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                    dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
             }
         }
         return dp[m][n];
+    }
+    
+    
+    int daily(string& word1, string& word2) {
+        int m = word1.length();
+        int n = word2.length();
+        
+        return m + n - 2 * lcs(word1, word2);
     }
 };
