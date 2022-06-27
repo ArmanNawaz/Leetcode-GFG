da1155e3198ffa09f6ed94913cca8cae8859bf15
ArmanNawaz <77883235+ArmanNawaz@users.noreply.github.com>
2022-06-27 11:35:48 +0530

    Time: 42 ms (13.82%), Space: 19.6 MB (58.46%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,22 +1,67 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        return solve(nums);
+        return moore(nums);
+        // return hashMap(nums);
+        // return sorting(nums);
+        // return bruteForce(nums);
     }
 private:
-    int solve(vector<int>& nums){
+    // O(n ^ 2) Time and O(1) extra space
+    int bruteForce(vector<int>& nums) {
         int n = nums.size();
-        int candidate = nums[0];
-        int fre = 1;
-        for(int j = 1; j < n; ++j){
-            if(candidate == nums[j])
-                ++fre;
-            else
-                --fre;
-            if(fre == 0 and j != n - 1)
-                candidate = nums[j+1];
-        }
         
+        for(int i = 0; i < n; ++i) {
+            int freq = 1;
+            for(int j = i + 1; j < n; ++j) {
+                if(nums[i] == nums[j])
+                    freq++;
+            }
+            if(freq > n / 2)
+                return nums[i];
+        }
+        return 0;        
+    }
+    
+    
+    // O(nlogn) Time and O(1) extra space
+    int sorting(vector<int>& nums) {
+        int n = nums.size();
+        
+        sort(nums.begin(), nums.end());
+        
+        return nums[n / 2];
+    }
+    
+    
+    // O(n) Time and O(n) extra space
+    int hashMap(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int, int> hashMap;
+        
+        for(int x: nums) {
+            hashMap[x]++;
+            if(hashMap[x] > n / 2)
+                return x;
+        }
+        return 0;
+    }
+    
+    
+    int moore(vector<int>& nums) {
+        int n = nums.size();
+        int vote = 1;
+        int candidate = nums[0];
+        
+        for(int i = 1; i < n; ++i) {
+            if(candidate == nums[i])
+                ++vote;
+            else
+                --vote;
+            
+            if(vote == 0 && i < n)
+                candidate = nums[i + 1];
+        }
         return candidate;
     }
 };
