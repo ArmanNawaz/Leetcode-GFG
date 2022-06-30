626ae1ea0dff99cbd5d54066e30a229c6b09e58d
ArmanNawaz <77883235+ArmanNawaz@users.noreply.github.com>
2022-06-30 12:16:14 +0530

    Time: 32 ms (5.90%), Space: 10.8 MB (59.74%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -4,19 +4,18 @@ public:
         return daily(nums);
     }
 private:
+    // O(n logn) Time and O(1) extra space
     int daily(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         int n = nums.size();
-        int req = 0;
-        if(n % 2 == 0)
-            req = (nums[n / 2 - 1] + nums[n / 2]) / 2;
-        else
-            req = nums[n / 2]; 
+        int req = nums[n / 2]; 
+        
         int ans = 0;
         
         for(int i = 0; i < n; ++i) {
             ans += abs(nums[i] - req);
         }
+        
         return ans;
     }
 };
