29980f22b6dfae81deaaba58ed5391a067883c1e
ArmanNawaz <77883235+ArmanNawaz@users.noreply.github.com>
2022-07-05 13:27:40 +0530

    Time: 46 ms (29.33%), Space: 13.1 MB (40.42%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -32,7 +32,20 @@ private:
         int take = recurseDp(text1, text2, i + 1, j, dp);
         int notTake = recurseDp(text1, text2, i, j + 1, dp);
 
-        return dp[i][j] = max(take, notTake);        
+        return dp[i][j] = max(take, notTake);
+    }
+    
+    int tabDp(string& text1, string& text2, int i, int j) {
+        vector<vector<int>> dp(text1.size() + 1, vector<int>(text2.size() + 1));
         
+        for(int i = 1; i <= text1.size(); ++i) {
+            for(int j = 1; j <= text2.size(); ++j) {
+                if(text1[i - 1] == text2[j - 1])
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                else
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[text1.size()][text2.size()];
     }
 };
