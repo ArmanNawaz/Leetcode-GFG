ef149bb8aa157027de6b75c287419acbf8bb6943
ArmanNawaz <77883235+ArmanNawaz@users.noreply.github.com>
2022-07-05 13:42:21 +0530

    Time: 30 ms (60.19%), Space: 6.5 MB (96.20%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -38,16 +38,20 @@ private:
     }
     
     int tabDp(string& text1, string& text2) {
-        vector<vector<int>> dp(text1.size() + 1, vector<int>(text2.size() + 1));
+        vector<vector<int>> dp(2, vector<int>(text2.size() + 1));
         
         for(int i = 1; i <= text1.size(); ++i) {
             for(int j = 1; j <= text2.size(); ++j) {
                 if(text1[i - 1] == text2[j - 1])
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    dp[i % 2][j] = dp[(i - 1) % 2][j - 1] + 1;
                 else
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                    dp[i % 2][j] = max(dp[(i - 1) % 2][j], dp[i % 2][j - 1]);
             }
         }
-        return dp[text1.size()][text2.size()];
+        // for(auto x: dp) {
+        //     for(auto y: x) cout<<y<<" ";
+        //     cout<<endl;
+        // }
+        return dp[text1.size() % 2][text2.size()];
     }
 };
