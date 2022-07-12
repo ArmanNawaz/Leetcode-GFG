c51877a0f1a74e8449c083fd0cc43582357d0c36
ArmanNawaz <77883235+ArmanNawaz@users.noreply.github.com>
2022-07-12 18:26:30 +0530

    Time: 49 ms (45.56%), Space: 13.6 MB (99.90%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,35 +1,28 @@
-class Solution {
-public:
-    vector<string> hashMap = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-    vector<string> letterCombinations(string digits) {
-        return solve(digits);
-    }
-private:
-    void backtrack(string& digits, int idx, string& partialAns, vector<string>& ans) {
-        // Base case
-        if(idx >= digits.length()) {
-            // here we have length of partialAns == length of digits
-            ans.push_back(partialAns); 
-            return;
-        }
+class Solution:
+    def solve(self, digits, idx, hashMap, partialAns, ans):
+   # base case
+        if idx >= len(digits):
+            # Append the current partial ans to ans
+            # here len(partialAns) == len(digits)
+            ans.append(''.join(partialAns))
+            return
         
-        // iterate through each character of current digit
-        for(auto ch: hashMap[digits[idx] - '2']) {
-            partialAns.push_back(ch); // append the current character to partial ans
-            backtrack(digits, idx + 1, partialAns, ans); // solve for next index
-            partialAns.pop_back();  // backtracking step
-        }
-    }
-    
-    vector<string> solve(string& digits) {
-        if(digits.length() == 0)
-            return {};
-        
-        
-        string partialAns = "";
-        vector<string> ans;
-        
-        backtrack(digits, 0, partialAns, ans);
-        return ans;
-    }
-};
+        # iterate through each character of the digit
+        for char in hashMap[digits[idx]]:
+            partialAns.append(char)  # use the current character
+            self.solve(digits, idx + 1, hashMap, partialAns, ans)  # move to the next digit
+
+            partialAns.pop()    # Backtracking step
+
+    def letterCombinations(self, digits: str) -> List[str]:
+        if len(digits) == 0:
+            return []
+        hashMap = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7': 'pqrs',
+                  '8': 'tuv', '9': 'wxyz'}
+
+        # to store partial answer until we found complete answer
+        partialAns = []
+        ans = []
+        self.solve(digits, 0, hashMap, partialAns, ans)
+        return ans
+  
