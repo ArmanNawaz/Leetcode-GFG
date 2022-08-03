d5d8f8dfd17fe28d22d572605391229eb2b35314
ArmanNawaz <77883235+ArmanNawaz@users.noreply.github.com>
2022-08-03 18:05:11 +0530

    Time: 4 ms (28.92%), Space: 6.4 MB (53.26%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
-        return aakashSir(m, n, 0, 0, dp);
+        // vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
+        // return aakashSir(m, n, 0, 0, dp);
+        
+        return aakashSirTab(m, n);
     }
 private:
     int aakashSir(int m, int n, int i, int j, vector<vector<int>>& dp) {
@@ -17,5 +19,23 @@ private:
         if(j >= n) return dp[i][j] = aakashSir(m, n, i + 1, j, dp);
         
         return dp[i][j] = aakashSir(m, n, i, j + 1, dp) + aakashSir(m, n, i + 1, j, dp);
+    }
+    
+    int aakashSirTab(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        
+        for(int i = 0; i < m; ++i)
+            dp[i][0] = 1;
+        
+        for(int i = 0; i < n; ++i)
+            dp[0][i] = 1;
+        
+        for(int i = 1; i < m; ++i) {
+            for(int j = 1; j < n; ++j) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        
+        return dp[m - 1][n - 1];
     }
 };
