1253406f2e845ba33a4ef8ffda4925c031ff40b0
ArmanNawaz <77883235+ArmanNawaz@users.noreply.github.com>
2022-08-04 12:08:14 +0530

    Time: 10 ms (80.68%), Space: 10 MB (43.63%) - LeetHub

--- 260-single-number-iii/260-single-number-iii.cpp
+++ 260-single-number-iii/260-single-number-iii.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        return solve(nums);        
+        return rahulSir(nums);
+        // return solve(nums);        
     }
 private:
     vector<int> solve(vector<int>& nums) {
@@ -16,5 +17,23 @@ private:
             if(entry.second == 1)
                 ans.push_back(entry.first);
         return ans;
+    }
+    
+    vector<int> rahulSir(vector<int>& nums) {
+        int bitmask = 0;
+        
+        for(int n: nums)
+            bitmask ^= n;
+        
+        int diff = bitmask & (-1 * (unsigned int)bitmask);
+        int y = 0;
+        
+        for(int n: nums) {
+            if((n & diff) != 0)
+                y ^= n;
+        }
+        int x = bitmask ^ y;
+        
+        return {x, y};
     }
 };
