a30eda0876d6ed597be60820ec843fd069915854
ArmanNawaz <77883235+ArmanNawaz@users.noreply.github.com>
2022-08-05 16:27:30 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (30.10%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -4,25 +4,25 @@ public:
         return solve(nums, target);
     }
 private:
-    int recurse(vector<int>& nums, int idx, int target, vector<vector<int>>& dp) {
+    int recurse(vector<int>& nums, int idx, int target, vector<int>& dp) {
         // base case
         if(target < 0) return 0;
         if(target == 0) {
             return 1;
         }
         
-        if(dp[idx][target] != -1)
-            return dp[idx][target];
+        if(dp[target] != -1)
+            return dp[target];
         
         int ans = 0;
         for(int i = 0; i < nums.size(); ++i) {
             ans += recurse(nums, i, target - nums[i], dp);
         }
-        return dp[idx][target] = ans;
+        return dp[target] = ans;
     }
     
     int solve(vector<int>& nums, int target) {
-        vector<vector<int>> dp(nums.size() + 1, vector<int>(target + 1, -1));
+        vector<int> dp(target + 1, -1);
         recurse(nums, 0, target, dp);
         
 //         for(auto x: dp) {
@@ -32,6 +32,6 @@ private:
 //         }
         
         
-        return dp[0][target];
+        return dp[target];
     }
 };
