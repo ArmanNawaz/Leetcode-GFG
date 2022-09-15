2b770740886f3cf654a29156e18aa5c26113ddfe
ArmanNawaz <77883235+ArmanNawaz@users.noreply.github.com>
2022-09-15 11:39:41 +0530

    Time: 492 ms (20.74%), Space: 48.8 MB (20.76%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        return solve(nums);
+        return rahulSir(nums);
+        // return solve(nums);
     }
 private:
     bool solve(vector<int>& nums) {
@@ -15,4 +16,22 @@ private:
         if(last <= 0) return true;
         return false;
     }
+    
+    bool rahulSir(vector<int>& nums) {
+        int n = nums.size();
+        vector<bool> dp(n, false);
+        
+        dp[n - 1] = true;
+        
+        for(int i = n - 2; i >= 0; --i) {
+            int farthestJump = min(i + nums[i], n - 1);
+            for(int j = i + 1; j <= farthestJump; ++j) {
+                if(dp[j] == true) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[0];
+    }    
 };
