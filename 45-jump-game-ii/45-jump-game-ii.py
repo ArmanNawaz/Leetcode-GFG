8692c7efd1fc7e4912caeb8f9022f1dc22aff907
ArmanNawaz <77883235+ArmanNawaz@users.noreply.github.com>
2022-09-20 11:47:31 +0530

    Time: 214 ms (64.87%), Space: 14.9 MB (90.08%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -1,15 +1,17 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
         n = len(nums)
-        i, currPos, jumps, maxJump = 0, 0, 0, 0
         
-        while currPos < n - 1:
-            maxJump = max(maxJump, i + nums[i])
-            
-            if i == currPos:
-                currPos = maxJump
+        if n == 1: return 0
+        
+        jumps = 1
+        maxReach = nums[0]
+        winEnd = nums[0]
+        
+        for i in range(1, n - 1):
+            maxReach = max(maxReach, i + nums[i])
+            if i == winEnd:
                 jumps += 1
-            i += 1
+                winEnd = maxReach
+        
         return jumps
-        
-        
