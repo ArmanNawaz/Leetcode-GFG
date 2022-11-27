5a3bc8ea956df2905c14fc1601e9ec924f928d63
ArmanNawaz <77883235+ArmanNawaz@users.noreply.github.com>
2022-11-27 22:48:06 +0530

    Added solution - LeetHub

---
+++ Check Arithmetic Progression - GFG/check-arithmetic-progression.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution{
+    public:
+    bool checkIsAP(int arr[], int n) {
+        sort(arr, arr + n);
+        
+        int cd = arr[1] - arr[0];
+        
+        for(int i = 2; i < n; ++i) {
+            if(arr[i] - arr[i - 1] != cd) return false;
+        }
+        
+        return true;
+    }
+
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        
+        Solution ob;
+         (ob.checkIsAP(arr, n))? (cout << "YES" << endl) :
+                       (cout << "NO" << endl);   
+    }
+ 
+  return 0;
+}
+
+// } Driver Code Ends
